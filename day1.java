6d8839f7947713025e2f3df7e35613ac50c58a99
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2024-08-01 10:06:30 +0100

    Create day1.java

---
+++ day1.java
@@ -0,0 +1,11 @@
+// HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
+
+/*
+Java was made by sun micro systems and has similiar syntax to c and c++.
+Java is an oopl and can run anywhere that has a jvm.
+*/
